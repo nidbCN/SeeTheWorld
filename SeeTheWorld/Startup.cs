7ea65ff5810d70c21245875b701dee14d9d32eb3
Gaein_nidb <mail@gaein.cn>
2021-08-12 01:13:03 +0800

    enable sqlite

--- SeeTheWorld/Startup.cs
+++ SeeTheWorld/Startup.cs
@@ -39,7 +39,7 @@ namespace SeeTheWorld
 
             services.AddDbContext<SeeTheWorldContext>(opt =>
             {
-                var connStr = Configuration.GetConnectionString("PostgreSql");
+                var connStr = Configuration.GetConnectionString(dbType);
 
                 _ = dbType.ToLower() switch
                 {
