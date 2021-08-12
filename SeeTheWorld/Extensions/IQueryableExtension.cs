1e7426fd875db21d3738e9e04b8f75dd8d0ccbe0
Gaein_nidb <mail@gaein.cn>
2021-08-12 17:13:15 +0800

    Add linq extension method

---
+++ SeeTheWorld/Extensions/IQueryableExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace SeeTheWorld.Extensions
+{
+    public static class IQueryableExtension
+    {
+        public static IQueryable<T> Random<T>(this IQueryable<T> source, int count)
+        {
+            if (count <= 0)
+                throw new ArgumentOutOfRangeException(nameof(count));
+
+            if (count == 0)
+                return source.Take(0);
+
+            var dataCount = source.Count();
+            if (count >= dataCount)
+                return source;
+
+            // [0, dataCount - count)
+            var indexRand = new Random().Next(0, dataCount - count);
+            return source.Skip(indexRand).Take(count);
+        }
+    }
+}
